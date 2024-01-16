6dc7753c7580fe8889f1b7f57e93e784a3abd03f
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-01-16 09:32:30 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by charlene hoareau on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
